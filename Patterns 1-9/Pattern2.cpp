1212be7137f60ec9a7ffa226f32c8cdb713e1a88
Pranav Avasthi <96632158+PranavAvasthi@users.noreply.github.com>
2022-06-14 17:47:44 +0530

    Add files via upload

---
+++ Patterns 1-9/Pattern2.cpp
@@ -0,0 +1,21 @@
+/* 
+Pattern 2 :-
+for n=3;
+1 1 1
+2 2 2 
+3 3 3 
+*/
+# include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=n; j++) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
